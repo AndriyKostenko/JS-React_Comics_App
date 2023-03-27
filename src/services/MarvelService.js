c2e4ead1193ff730ebbf577241f902d709dafcac
Andriy Kostenko <a.kostenkouk@gmail.com>
2023-03-26 20:12:59 -0400

    updated service func., optimized code for getting API

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,4 +1,3 @@
-
 // getting info from API
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
@@ -14,12 +13,24 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await  this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter); // appliying for each received data from response Object
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?limit=9&offset=210&${this._apiKey}`)
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?limit=9&offset=210&${this._apiKey}`)
+        return this._transformCharacter(res.data.results[0]); // get a character object
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 
 
