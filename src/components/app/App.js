a5b177865ff4b5e2a75f4acd7bea44ad8c5e48d7
Andriy Kostenko <a.kostenkouk@gmail.com>
2023-03-28 20:59:17 -0400

    added func. for cathing the error through the ErrorBoundary component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,8 +4,10 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
+
 
 class App extends Component {
 
@@ -28,7 +30,10 @@ class App extends Component {
                     <RandomChar/>
                     <div className="char__content">
                         <CharList onCharSelected={this.onCharSelected}/>
-                        <CharInfo charId={this.state.selectedChar}/>
+                        <ErrorBoundary>
+                            <CharInfo charId={this.state.selectedChar}/>
+                        </ErrorBoundary>
+                        
                     </div>
                     <img className="bg-decoration" src={decoration} alt="vision"/>
                 </main>
